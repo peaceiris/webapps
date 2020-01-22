25438d78850808a9c3dcff635aa503dc587a4902
peaceiris <30958501+peaceiris@users.noreply.github.com>
2020-01-23 01:33:57 +0900

    fix: Add QR Code logo

--- pages/qrcode-reader.vue
+++ pages/qrcode-reader.vue
@@ -1,7 +1,7 @@
 <template>
   <v-layout>
     <v-flex class="text-center">
-      <img src="/logo_qrcode.png" alt="QR Code" class="mb-5" />
+      <img src="/images/logo_qrcode.png" alt="QR Code" class="mb-5" />
     </v-flex>
   </v-layout>
 </template>
