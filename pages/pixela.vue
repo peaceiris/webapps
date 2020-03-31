2a3c318d8ff8a7b41218cd6eb2a14f4e35ccef11
peaceiris <30958501+peaceiris@users.noreply.github.com>
2020-04-01 01:39:03 +0900

    feat: Add nomal and current input

--- pages/pixela.vue
+++ pages/pixela.vue
@@ -17,7 +17,7 @@
         <v-container fluid>
           <v-row>
             <v-col cols="12" sm="6" md="6">
-              <v-text-field label="Username" :rules="[rules.required]" />
+              <v-text-field label="pixela-username" :rules="[rules.required]" />
             </v-col>
             <v-col cols="12" sm="6" md="6">
               <v-text-field
@@ -25,11 +25,30 @@
                 :rules="[rules.required, rules.min]"
                 :type="token ? 'text' : 'password'"
                 name="input-10-2"
-                label="Token"
+                label="pixela-token"
                 hint="Your Pixela Token Here"
                 value=""
                 class="input-group--focused"
                 @click:append="token = !token"
+              />
+            </v-col>
+            <v-col cols="12" sm="6" md="6">
+              <v-text-field
+                label="Normal Temperature"
+                hint="Your Normal Temperature Here"
+                :value="nomal"
+                :rules="[rules.required]"
+                @change="setNomal"
+              />
+            </v-col>
+            <v-col cols="12" sm="6" md="6">
+              <v-select
+                v-model="defaultSelected"
+                :items="items"
+                item-text="label"
+                item-value="value"
+                label="Current Temperature"
+                hint="Your Current Temperature Here"
               />
             </v-col>
           </v-row>
@@ -40,6 +59,18 @@
 </template>
 
 <script>
+const nomalTemp = 36.9
+
+function getTempItems() {
+  const items = []
+
+  for (let i = 0; i < 50; i++) {
+    const j = (i * 0.1 + 35.5).toFixed(1)
+    items.push({label: `${j}`, value: `${j}`})
+  }
+  return items
+}
+
 export default {
   data() {
     return {
@@ -48,7 +79,15 @@ export default {
       rules: {
         required: (value) => !!value || 'Required.',
         min: (v) => v.length >= 50 || 'Min 50 characters'
-      }
+      },
+      nomal: `${nomalTemp}`,
+      defaultSelected: {label: `${nomalTemp}`, value: `${nomalTemp}`},
+      items: getTempItems()
+    }
+  },
+  methods: {
+    setNomal(nomal) {
+      this.nomal = nomal
     }
   }
 }
